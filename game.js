bbec57c7f3165cdf3cc08787fa8b84c561d45e0d
ajhowell2000 <anazjai@gmail.com>
2020-03-04 22:21:20 -0500

    Interactive

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ d3.select("body")
         .text("GUESSING GAME")
 d3.select("body")
     .append("p")
-        .text("how to play: hover your mouse over an image and click to see the options:Cow,Pig,Chicken!")
+        .text("how to play: make a guess of what is behind the card: a cow, a pig, or chicken. Check your guess by clicking on the card. Can you guess correctly?")
         .on("mouseover",function()
         {
             d3.select(this)
@@ -14,11 +14,9 @@ d3.select("body")
         .on("mouseleave", function()
         {
             d3.select(this)
-                .text("how to play:hover your mouse over an image and click to see the options:Cow,Pig,Chicken! ");
+                .text("how to play: make a guess of what is behind the card: a cow, a pig, or chicken. Check your guess by clicking on the card. Can you guess correctly?");
         
         })
-d3.select("body")
-.append("br")
 d3.select("body")
     .append("img")
         .attr("src","pics/blackcard.jpg")
@@ -44,19 +42,22 @@ d3.select("body")
      .attr("width","100px")
         .attr("height","140px")
 })
-
 d3.select("body")
-        .append("img")
-        .attr("src","pics/blackcard.jpg")
+        .append("img")   
+    .attr("src","pics/blackcard.jpg")
         .attr("width","100px")
         .attr("height","140px")
-    .on("click",function()
-        {
+        
+    .on("click",function()  
+   {
         d3.select("body")
     .append("img")
     .attr("src","pics/Busteni_-_chicken.jpg")
      .attr("width","100px")
         .attr("height","140px")
 })
+
+ d3.select("body")
+.append("br")
 
 
